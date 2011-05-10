fa1111272b514dbee28b2d145e3f766bca23e95e
Mason Fischer <moocowmason@gmail.com>
2011-05-10 18:43:08 -0400

    Turned on Cacheing?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Kissr::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  config.cache_store = :dalli_store
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
