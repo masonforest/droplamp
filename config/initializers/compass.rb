6c575b1236cb58a5600df03fbeb462f27f4abc58
Mason Fischer <moocowmason@gmail.com>
2011-06-08 11:53:49 -0400

    compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,12 +1,12 @@
 require 'fileutils'
 FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
-Compass::AppIntegration::Rails.initialize!
+#Compass::AppIntegration::Rails.initialize!
 
-Rails.configuration.middleware.delete('Sass::Plugin::Rack')
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+#Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+#Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
 
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
-    :urls => ['/stylesheets'],
-    :root => "#{Rails.root}/tmp")
-
+#Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+#    :urls => ['/stylesheets'],
+#    :root => "#{Rails.root}/tmp")
+ActionController::Dispatcher.middleware.use(Rack::Static, :root => "tmp/stylesheets", :urls => ["/stylesheets/"])
