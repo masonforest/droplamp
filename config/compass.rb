8f293acf2365a5e52ab2110095dad181ce82b049
Mason Fischer <moocowmason@gmail.com>
2011-06-08 12:29:12 -0400

    compass

--- config/compass.rb
+++ config/compass.rb
@@ -1,8 +1,11 @@
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
-environment  = Compass::AppIntegration::Rails.env
-preferred_syntax=:sass
-http_path = '/'
-css_dir   = 'tmp/stylesheets'
-sass_dir  = 'app/views/stylesheets'
+http_path = "/"
+sass_dir = "app/views/stylesheets"
+environment = Compass::AppIntegration::Rails.env
+if environment == 'production'
+  css_dir = "tmp/stylesheets"
+else
+  css_dir = "public/stylesheets"
+end
 
--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,17 @@
-Rails.configuration.middleware.delete('Sass::Plugin::Rack')
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+require 'compass'
+require 'compass/app_integration/rails'
 
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
-    :urls => ['/stylesheets'],
-    :root => "#{Rails.root}/tmp")
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+environment = Compass::AppIntegration::Rails.env
+if environment == 'production'
+  Compass::AppIntegration::Rails.initialize!
+
+  Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+  Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+
+  Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+      :urls => ['/stylesheets'],
+      :root => "#{Rails.root}/tmp")
+end
