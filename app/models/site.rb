c533d679a4572f1e34d126020e46e638007e9ae8
Mason Fischer <mason@stirltech.com>
2011-11-13 19:46:25 -0500

    updated site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,4 @@
+require 'dropbox_sdk'
 class Site < ActiveRecord::Base
   after_create :create_heroku_domain,:create_dropbox_folder
   belongs_to :user
@@ -15,25 +16,32 @@ class Site < ActiveRecord::Base
   end
 
   def create_dropbox_folder
-    path=self.dropbox_folder
-    puts self.user.inspect
-    # TODO add upload folder method to dropbox gem
-    dropbox.create_folder(path)
-    dropbox.create_folder(path+'/css')
-    dropbox.create_folder(path+'/_layouts')
-    dropbox.upload( Rails.root.join("templates","default", "index.md").to_s ,path)
-    dropbox.upload( Rails.root.join("templates","default",  "about.md").to_s ,path)
-    dropbox.upload( Rails.root.join("templates","default",  "contact.md").to_s ,path)
-    dropbox.upload( Rails.root.join("templates","default",  "_layouts","default.html").to_s ,path+"/_layouts")
-    dropbox.upload( Rails.root.join("templates","default",  "css","style.css").to_s ,path+'/css')
-    dropbox.upload( Rails.root.join("templates","default",  "css","screen.css").to_s ,path+'/css')
+    Dir["#{Rails.root}/templates/default/**/**"].each do |file|
+      next if File.directory?(file)
+      to_path=self.dropbox_folder+file.sub("#{Rails.root}/templates/default","")
+      dropbox.put_file( to_path,file )
+    end
+
+    #path=self.dropbox_folder
+    #puts self.user.inspect
+    #Rails.root.join("templates","default")
+    # TODO add put_file folder method to dropbox gem
+    #dropbox.file_create_folder(path)
+    #dropbox.file_create_folder(path+'/css')
+    #dropbox.file_create_folder(path+'/_layouts')
+    #dropbox.put_file( path+ Rails.root.join("templates","default", "index.md").to_s)
+    #dropbox.put_file( Rails.root.join("templates","default",  "about.md").to_s )
+    #dropbox.put_file( Rails.root.join("templates","default",  "contact.md").to_s )
+    #dropbox.put_file( Rails.root.join("templates","default",  "_layouts","default.html").to_s)
+    #dropbox.put_file( Rails.root.join("templates","default",  "css","style.css").to_s )
+    #dropbox.put_file( Rails.root.join("templates","default",  "css","screen.css").to_s )
     
   end
   def dropbox
    @dropbox ||= begin
-    dropbox=Dropbox::Session.new(ENV['DROPBOX_KEY'], ENV['DROPBOX_SECRET'])
-    dropbox.set_access_token(self.owner.dropbox_token,self.owner.dropbox_token_secret)
-    dropbox.mode = :dropbox
+    session=DropboxSession.new(ENV['DROPBOX_KEY'], ENV['DROPBOX_SECRET'])
+    session.set_access_token(self.owner.dropbox_token,self.owner.dropbox_token_secret)
+    dropbox=DropboxClient.new(session,:app_folder)
     dropbox 
    end
   end
