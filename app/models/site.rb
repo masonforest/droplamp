992f80a8fe80d9e4123b2ff874be8a1a562808d0
Mason Fischer <mason@stirltech.com>
2011-11-14 12:14:16 -0500

    updated dropbox upload method calls

--- app/models/site.rb
+++ app/models/site.rb
@@ -19,23 +19,8 @@ class Site < ActiveRecord::Base
     Dir["#{Rails.root}/templates/default/**/**"].each do |file|
       next if File.directory?(file)
       to_path=self.dropbox_folder+file.sub("#{Rails.root}/templates/default","")
-      dropbox.put_file( to_path,file )
+      dropbox.put_file( to_path,File.new(file,"r") )
     end
-
-    #path=self.dropbox_folder
-    #puts self.user.inspect
-    #Rails.root.join("templates","default")
-    # TODO add put_file folder method to dropbox gem
-    #dropbox.file_create_folder(path)
-    #dropbox.file_create_folder(path+'/css')
-    #dropbox.file_create_folder(path+'/_layouts')
-    #dropbox.put_file( path+ Rails.root.join("templates","default", "index.md").to_s)
-    #dropbox.put_file( Rails.root.join("templates","default",  "about.md").to_s )
-    #dropbox.put_file( Rails.root.join("templates","default",  "contact.md").to_s )
-    #dropbox.put_file( Rails.root.join("templates","default",  "_layouts","default.html").to_s)
-    #dropbox.put_file( Rails.root.join("templates","default",  "css","style.css").to_s )
-    #dropbox.put_file( Rails.root.join("templates","default",  "css","screen.css").to_s )
-    
   end
   def dropbox
    @dropbox ||= begin
