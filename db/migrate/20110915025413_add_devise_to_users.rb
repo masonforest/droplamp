60b38b1eb6e2c82c07a19e6d69adbeb1a3a57750
Mason Fischer <mason@stirltech.com>
2012-02-26 15:58:04 -0500

    unlock devise

--- db/migrate/20110915025413_add_devise_to_users.rb
+++ db/migrate/20110915025413_add_devise_to_users.rb
@@ -1,26 +1,26 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
-      #t.database_authenticatable :null => false
-      #t.recoverable
-      #t.rememberable
-      #t.trackable
+      t.database_authenticatable :null => false
+      t.recoverable
+      t.rememberable
+      t.trackable
     
-      # t.encryptable
+       t.encryptable
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-      # t.token_authenticatable
+       t.token_authenticatable
 
 
       # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps
+       t.timestamps
     end
 
-    #add_index :users, :email,                :unique => true
-    #add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
-    # add_index :users, :unlock_token,         :unique => true
-    # add_index :users, :authentication_token, :unique => true
+    add_index :users, :email,                :unique => true
+    add_index :users, :reset_password_token, :unique => true
+     add_index :users, :confirmation_token,   :unique => true
+     add_index :users, :unlock_token,         :unique => true
+     add_index :users, :authentication_token, :unique => true
   end
 
   def self.down
